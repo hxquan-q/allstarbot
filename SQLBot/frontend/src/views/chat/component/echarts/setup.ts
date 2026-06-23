3b33539b56075c2557cb817eeaffe4ff8497b85d
“xquan” <2763853225@qq.com>
2026-06-23 22:01:35 +0800

    feat(chart): 引入 echarts + vue-echarts 并注册 SVG 渲染器

---
+++ SQLBot/frontend/src/views/chat/component/echarts/setup.ts
@@ -0,0 +1,26 @@
+import { use } from 'echarts/core'
+import { SVGRenderer } from 'echarts/renderers'
+import { BarChart, LineChart, PieChart, ScatterChart } from 'echarts/charts'
+import {
+  GridComponent,
+  TooltipComponent,
+  LegendComponent,
+  TitleComponent,
+  DatasetComponent,
+} from 'echarts/components'
+import VChart from 'vue-echarts'
+
+use([
+  SVGRenderer,
+  BarChart,
+  LineChart,
+  PieChart,
+  ScatterChart,
+  GridComponent,
+  TooltipComponent,
+  LegendComponent,
+  TitleComponent,
+  DatasetComponent,
+])
+
+export default VChart
